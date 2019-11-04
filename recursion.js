d3c36bd199d1b977664dbf7ba8c62c28dd3b24b9
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-04 14:18:36 -0800

    complete anagrams

--- recursion.js
+++ recursion.js
@@ -110,4 +110,25 @@ const findWay = function(arrayMaze,result=''){
   
 };
 
-console.log(findWay(mySmallMaze));
+// console.log(findWay(mySmallMaze));
+
+const anagrams = function(string) {
+  var results = [];
+
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    var firstChar = string[i];
+    var charsLeft = string.substring(0, i) + string.substring(i + 1);
+    var innerPermutations = anagrams(charsLeft);
+    for (var j = 0; j < innerPermutations.length; j++) {
+      results.push(firstChar + innerPermutations[j]);
+    }
+  }
+  return results;
+}
+
+console.log(anagrams('cat'));
